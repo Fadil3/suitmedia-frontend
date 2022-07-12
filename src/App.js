a191ca80e9efe476f2aa2734115189f49d981f11
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-07-12 22:07:09 +0700

    import contact module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
 import { Footer } from './components'
+import { ContactModule } from './modules'
 
 function App() {
   return (
     <div className="App">
+      <ContactModule />
       <Footer />
     </div>
   )
