e847c0c6f22bc99177c65994b8ba01b8bcad612f
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-07-12 20:51:04 +0700

    create footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,34 @@
+import styles from './Footer.module.css'
+import { icons } from '../../constants'
+
+export default function Footer() {
+  const social = [
+    {
+      name: 'facebook',
+      icon: icons.facebookOfficial,
+      href: 'https://www.facebook.com/suitmedia',
+    },
+    {
+      name: 'twitter',
+      icon: icons.twitter,
+      href: 'https://www.twitter.com/suitmedia',
+    },
+  ]
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright &copy; 2016. PT Company</p>
+      <div className={styles.footer__social}>
+        {social.map((item, index) => (
+          <a
+            href={item.href}
+            key={'footer' + index}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={item.icon} alt={item.name} />
+          </a>
+        ))}
+      </div>
+    </footer>
+  )
+}
