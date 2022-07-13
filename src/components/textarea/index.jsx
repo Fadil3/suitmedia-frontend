3c2653648f2a9166d36d88bc7730da473b257c59
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-07-13 09:57:48 +0700

    handle textarea event

--- src/components/textarea/index.jsx
+++ src/components/textarea/index.jsx
@@ -1,19 +1,22 @@
 import './Textarea.css'
 
 export default function Textarea({
-  type,
   title,
   name,
-  placeholder,
-  value,
-  onChange,
+  handleChange,
   error,
   ...props
 }) {
   return (
     <div div className="textarea__container">
       <label htmlFor={name}>{title}</label>
-      <textarea {...props}></textarea>
+      <textarea
+        style={{ borderColor: error !== '' ? 'red' : '#ccc' }}
+        name={name}
+        onChange={handleChange}
+        {...props}
+      ></textarea>
+      {error && <span className="textarea__error">{error}</span>}
     </div>
   )
 }
