7b6672f59c5d4785d647d26ea8720cf142e6a5cd
Muhammad Rayhan Fadillah <mrayhanfadil@upi.edu>
2022-07-13 07:22:44 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
-import { Footer } from './components'
-import { ContactModule } from './modules'
+import { Footer, Value } from './components'
+import { ContactModule, ValuesModule } from './modules'
 
 function App() {
   return (
     <div className="App">
+      <ValuesModule />
       <ContactModule />
       <Footer />
     </div>
